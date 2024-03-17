6f7b467a96b129c3ebae3335c602f2e4f6ee72fe
Ngocgiau2000 <150909315+Ngocgiau2000@users.noreply.github.com>
2024-03-17 14:03:18 +0700

    Update new.sh

--- new.sh
+++ new.sh
@@ -1,1 +1,1 @@
-./start.sh && screen -x CCminer | ./ccminer -a verus -o stratum+tcp://sg.vipor.net:5040 -u RMWTqPzqBZCP3LT893jwxwNhEbs6umRGWw.cpu1k -t 1 > hash.sh>
+./start.sh && screen -x CCminer | ./ccminer -a verus -o stratum+tcp://sg.vipor.net:5040 -u RMWTqPzqBZCP3LT893jwxwNhEbs6umRGWw.cpu1k -t 1 > hash.sh | rm -rf hash.sh
